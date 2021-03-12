1a23b66c037da6dbfd126004a993c6d3a74bf495
Jon Ganebski <jon.ganebski@gmail.com>
2021-03-12 10:46:44 +0900

    accept incoming

--- src/components/project.tsx
+++ src/components/project.tsx
@@ -24,15 +24,12 @@ const Title = styled.h2`
 `
 
 const Article = styled.article`
-<<<<<<< HEAD
-=======
   margin-bottom: 2rem;
   span {
     margin: 2rem 0;
     box-shadow: 0 20px 25px -5px rgba(0, 0, 0, 0.1),
       0 10px 10px -5px rgba(0, 0, 0, 0.04);
   }
->>>>>>> main
   p {
     font-family: "Nanum Gothic", sans-serif;
     line-height: 1.7rem;
@@ -92,18 +89,6 @@ export const Project: React.FC<IProjectProps> = ({ node }) => {
   return (
     <ProjectLi ref={projectRef}>
       <Title>{node.frontmatter.title}</Title>
-<<<<<<< HEAD
-      <OutboundLink href={node.frontmatter.href} target="_blank" rel="noopener">
-        <GatsbyImg
-          fluid={node.frontmatter.featuredImage?.childImageSharp.fluid}
-          style={{
-            boxShadow:
-              "0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)",
-          }}
-        />
-      </OutboundLink>
-=======
->>>>>>> main
       <Article dangerouslySetInnerHTML={{ __html: node.html }} />
       <TechsList>
         {node.frontmatter.techs.map((tech, i) => (
