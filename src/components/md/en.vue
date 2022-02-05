6e4add1b1119f2dba65418a5a77a814acbb9e8c8
jonganebski <jon.ganebski@gmail.com>
2022-02-05 22:51:04 +0900

    :sparkles: create markdown components

---
+++ src/components/md/en.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+const props = withDefaults(defineProps<{ as: keyof HTMLElementTagNameMap }>(), { as: 'div' });
+
+const { locale } = useI18n();
+</script>
+
+<template>
+	<props.as v-show="locale === 'en'"> <slot /> </props.as>
+</template>
---
+++ src/components/md/wrapper.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = defineProps<{ frontmatter: { [key: string]: any } }>();
+</script>
+
+<template>
+	<div class="ml-14 p-20 bg-yellow-100">
+		<article class="py-10 max-w-screen-lg bg-white">
+			<md-ko as="h1" class="mx-16 text-7xl font-bold">
+				{{ props.frontmatter.title.ko }}
+			</md-ko>
+			<md-en as="h1" class="mx-16 text-7xl font-bold">
+				{{ props.frontmatter.title.en }}
+			</md-en>
+			<img class="mt-8" :src="props.frontmatter.cover_image_url" alt="" />
+			<div class="mt-24">
+				<slot />
+			</div>
+		</article>
+	</div>
+</template>
+
+<style scoped lang="css">
+::v-slotted(img) {
+	width: 100%;
+}
+</style>
