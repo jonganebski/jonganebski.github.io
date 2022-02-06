fa071307d5ebda639f913180bb65b296da8b7093
jonganebski <jon.ganebski@gmail.com>
2022-02-06 17:32:15 +0900

    :wrench: add aspect-ratio plugin

--- windi.config.ts
+++ windi.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'windicss/helpers';
 import typography from 'windicss/plugin/typography';
+import aspectRatio from 'windicss/plugin/aspect-ratio';
 
 export const markdownWrapperClass = 'prose prose m-auto text-left';
 
@@ -7,5 +8,5 @@ export default defineConfig({
 	attributify: true,
 	darkMode: 'class',
 	safelist: [markdownWrapperClass],
-	plugins: [typography()],
+	plugins: [typography(), aspectRatio],
 });
