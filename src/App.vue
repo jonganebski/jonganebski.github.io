1760f2b7b6bca5cb73d7b65a0613c1d3940e45b5
jonganebski <jon.ganebski@gmail.com>
2022-02-08 14:27:30 +0900

    :sparkles: add vue-query

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { VueQueryDevTools } from 'vue-query/devtools';
+</script>
 
 <template>
 	<router-view />
+	<VueQueryDevTools />
 </template>
