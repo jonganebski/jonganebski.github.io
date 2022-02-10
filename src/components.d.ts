72916d6682e4c0bef3e1d1c057648ba550b3e564
jonganebski <jon.ganebski@gmail.com>
2022-02-10 10:53:44 +0900

    :label: autogenerated

--- src/components.d.ts
+++ src/components.d.ts
@@ -3,11 +3,12 @@
 // Read more: https://github.com/vuejs/vue-next/pull/3399
 
 declare module 'vue' {
-  export interface GlobalComponents {
-    MdEn: typeof import('./components/md/en.vue')['default']
-    MdKo: typeof import('./components/md/ko.vue')['default']
-    MdWrapper: typeof import('./components/md/wrapper.vue')['default']
-  }
+	export interface GlobalComponents {
+		MdEn: typeof import('./components/md/en.vue')['default'];
+		MdKo: typeof import('./components/md/ko.vue')['default'];
+		MdWrapper: typeof import('./components/md/wrapper.vue')['default'];
+		UiBlock: typeof import('./components/ui/block.vue')['default'];
+	}
 }
 
-export { }
+export {};
