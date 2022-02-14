6e1cc885a2b7a2f33ae9742889d66dca41bfb2c1
jonganebski <jon.ganebski@gmail.com>
2022-02-14 19:36:27 +0900

    :sparkles: create useMyI18n

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,20 +1,28 @@
 import { createI18n } from 'vue-i18n';
 import type { ViteSSGContext } from 'vite-ssg';
+import koMessages from '../../locales/ko.json';
+import enMessages from '../../locales/en.json';
 
 type Plugin = (ctx: ViteSSGContext) => void;
 
-const messages = Object.fromEntries(
-  Object.entries(import.meta.globEager('../../locales/*.json')).map(([key, value]) => {
-    const json = key.endsWith('.json');
-    return [key.slice(14, json ? -5 : -4), value.default];
-  }),
-);
+export type Locale = 'en' | 'ko';
+
+// const messages = Object.fromEntries(
+//   Object.entries(import.meta.globEager('../../locales/*.json')).map(([key, value]) => {
+//     const json = key.endsWith('.json');
+//     return [key.slice(14, json ? -5 : -4), value.default];
+//   }),
+// );
+
+export function useMyI18n() {
+  return useI18n<[typeof enMessages], Locale>();
+}
 
 export const install: Plugin = ({ app }) => {
   const i18n = createI18n({
     legacy: false,
     locale: 'en',
-    messages,
+    messages: { ko: koMessages, en: enMessages },
   });
 
   app.use(i18n);
