9c89c3d328022a30d9d4d0c72396d8255d6f2d37
jonganebski <jon.ganebski@gmail.com>
2022-02-16 20:27:22 +0900

    :sparkles: update index page

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,9 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useMyI18n } from '~/plugins/i18n';
+
+const { t } = useMyI18n();
+
+const cloudShadows = ref(false);
+</script>
 
 <template>
-  <div>
-    <div class="h-96 bg-white"></div>
-    <div class="h-96 bg-teal-700"></div>
-    <div class="h-96 bg-indigo-900"></div>
+  <section class="max-w-screen-md pt-40 mx-auto">
+    <p>{{ t('hello') }}! 👋</p>
+    <p>{{ t('welcome_to_my_blog') }}</p>
+    <p>{{ t('i_am_a_frontend_developer_and_a_traveler') }}</p>
+  </section>
+  <div v-if="cloudShadows">
+    <ui-cloud-shadow :delay="0" />
+    <ui-cloud-shadow :delay="10" />
+    <ui-cloud-shadow :delay="20" />
+    <ui-cloud-shadow :delay="30" />
   </div>
+
+  <ui-contour-lines :draw="true" @on-transition-end="cloudShadows = true" />
 </template>
