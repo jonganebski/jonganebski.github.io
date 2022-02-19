7461363655f9d8e8dc29ba5558af3c7ea3bffa62
jonganebski <jon.ganebski@gmail.com>
2022-02-19 12:09:43 +0900

    :lipstick: update posts/techs page

--- src/pages/posts/techs/index.vue
+++ src/pages/posts/techs/index.vue
@@ -54,31 +54,37 @@ onMounted(() => {
   <div class="mt-20 mb-40 px-16 mx-auto">
     <div class="flex justify-between">
       <div>
-        <h1 class="text-3xl">기술 블로그</h1>
-        <p>주로 자바스크립트에 대해 다루고 있습니다.</p>
+        <h1 class="text-6xl">{{ t('tech_blog') }}</h1>
+        <p class="mt-10">{{ t('posts_techs.p_1') }}</p>
+        <p>{{ t('posts_techs.p_2') }}</p>
+        <p>{{ t('posts_techs.p_3') }}</p>
       </div>
-      <router-link to="/notes/tech">Notes</router-link>
+      <router-link to="/notes/tech">{{ t('note', 2) }}</router-link>
     </div>
-    <ul class="mt-48 space-y-32">
+    <ul class="mt-32 space-y-32">
       <li
         v-for="({ cover_image_url, fileName, title, date }, index) in posts"
         :key="index"
-        class="max-w-6xl flex"
+        class="relative max-w-6xl flex"
       >
         <img
           :data-src="cover_image_url"
-          :ref="(x) => imageRefs.push(x as HTMLImageElement)"
+          :ref="(el) => imageRefs.push(el as HTMLImageElement)"
           class="h-[300px] aspect-video object-cover bg-gray-300"
           width="500"
           height="300"
           loading="lazy"
         />
-        <div class="px-16 max-w-lg flex-1">
+        <div class="z-1 px-16 max-w-lg flex-1">
           <p class="text-xs mr-auto">
             {{ formatDate(date) }}
           </p>
           <h5 class="mt-6 text-5xl">{{ title[locale] }}</h5>
-          <ui-party-btn :content="t('click')" :to="`/posts/techs/${fileName}`" class="mt-10" />
+          <ui-party-btn
+            :content="t('click').toUpperCase()"
+            :to="`/posts/techs/${fileName}`"
+            class="mt-10 text-sm"
+          />
         </div>
         <span
           :ref="(el) => iconContainerRefs.push(el as HTMLSpanElement)"
