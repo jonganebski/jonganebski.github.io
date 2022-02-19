91f4cf1f24a098661262f67e9e8bcf04a6ffd452
jonganebski <jon.ganebski@gmail.com>
2022-02-19 20:06:36 +0900

    :art: update env lib

--- src/libs/env.ts
+++ src/libs/env.ts
@@ -1,3 +1,5 @@
-export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
 
-export const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+export const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
+
+export const mapboxAccessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN as string;
