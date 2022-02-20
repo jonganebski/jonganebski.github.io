77706ab272c228b32101bd9fbc010fad48d6316d
jonganebski <jon.ganebski@gmail.com>
2022-02-20 12:54:53 +0900

    :art: update md-wrapper component

--- src/components/md/wrapper.vue
+++ src/components/md/wrapper.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
+import { Head } from '@vueuse/head';
 import type { Frontmatter } from '~/libs/markdown';
+import { useMyI18n } from '~/plugins/i18n';
 
 interface Props {
   frontmatter: Frontmatter;
@@ -9,17 +11,19 @@ const props = defineProps<Props>();
 
 // This doesn't need to be a computedRef because this component is static.
 const { frontmatter } = props;
+
+const { locale } = useMyI18n();
 </script>
 
 <template>
+  <Head>
+    <title>{{ frontmatter.title[locale] }} | Jon Ganebski</title>
+  </Head>
   <div class="p-20 bg-yellow-100">
     <article class="py-10 max-w-screen-lg bg-white">
-      <md-ko as="h1" class="mx-16 text-7xl font-bold">
-        {{ frontmatter.title.ko }}
-      </md-ko>
-      <md-en as="h1" class="mx-16 text-7xl font-bold">
-        {{ frontmatter.title.en }}
-      </md-en>
+      <h1 class="mx-16 text-7xl font-bold">
+        {{ frontmatter.title[locale] }}
+      </h1>
       <img
         v-if="frontmatter.cover_image_url"
         class="mt-8"
