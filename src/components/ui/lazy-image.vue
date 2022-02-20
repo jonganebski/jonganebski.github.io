688865e981a3b263e669443ae389361fe10cb9c8
jonganebski <jon.ganebski@gmail.com>
2022-02-20 13:44:59 +0900

    :zap: update ui-lazy-image component

--- src/components/ui/lazy-image.vue
+++ src/components/ui/lazy-image.vue
@@ -1,17 +1,32 @@
 <script setup lang="ts">
-import { useElementVisibility } from '@vueuse/core';
+import { useIntersectionObserver } from '@vueuse/core';
 
 interface Props {
+  height?: number;
+  width?: number;
   src: string;
 }
 
-const props = defineProps<Props>();
+const props = withDefaults(defineProps<Props>(), { height: 400, width: 600 });
 
 const imgRef = ref<HTMLImageElement | null>(null);
 
-const isVisible = useElementVisibility(imgRef);
+const { stop } = useIntersectionObserver(imgRef, (entries) => {
+  const { isIntersecting } = entries[0];
+  if (!isIntersecting) return;
+  if (!imgRef.value?.dataset.src) return;
+  imgRef.value.src = imgRef.value.dataset.src;
+  stop();
+});
 </script>
 
 <template>
-  <img ref="imgRef" :src="isVisible ? props.src : undefined" />
+  <img
+    ref="imgRef"
+    :data-src="props.src"
+    :height="props.height"
+    :width="props.width"
+    class="bg-gray-400"
+    loading="lazy"
+  />
 </template>
