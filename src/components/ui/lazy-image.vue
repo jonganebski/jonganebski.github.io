9500ce0ca858874411b8bddc64a857861a002f52
jonganebski <jon.ganebski@gmail.com>
2022-02-20 12:53:09 +0900

    :sparkles: create ui-lazy-image

---
+++ src/components/ui/lazy-image.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useElementVisibility } from '@vueuse/core';
+
+interface Props {
+  src: string;
+}
+
+const props = defineProps<Props>();
+
+const imgRef = ref<HTMLImageElement | null>(null);
+
+const isVisible = useElementVisibility(imgRef);
+</script>
+
+<template>
+  <img ref="imgRef" :src="isVisible ? props.src : undefined" />
+</template>
