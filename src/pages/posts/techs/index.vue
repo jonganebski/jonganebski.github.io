2c369667ae7c070eb7697d4427f5b20349502bae
jonganebski <jon.ganebski@gmail.com>
2022-02-20 12:56:10 +0900

    :bug: fix incompatible with ssg bug

--- src/pages/posts/techs/index.vue
+++ src/pages/posts/techs/index.vue
@@ -1,18 +1,10 @@
 <script setup lang="ts">
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { getTechPosts } from '~/libs/markdown';
-import { computeRandInt } from '~/libs/random';
 import { useMyI18n } from '~/plugins/i18n';
-
-gsap.registerPlugin(ScrollTrigger);
 
 const { locale, t } = useMyI18n();
 
 const posts = getTechPosts()?.sort((a, b) => b.date.localeCompare(a.date));
-
-const imageRefs = ref<HTMLImageElement[]>([]);
-const iconContainerRefs = ref<HTMLSpanElement[]>([]);
 
 function formatDate(dateString: string) {
   return new Date(dateString).toLocaleDateString(locale.value, {
@@ -21,33 +13,6 @@ function formatDate(dateString: string) {
     day: '2-digit',
   });
 }
-
-onMounted(() => {
-  imageRefs.value.forEach((imageElement) => {
-    ScrollTrigger.create({
-      trigger: imageElement,
-      onEnter: () => {
-        imageElement.src = imageElement.dataset.src ?? '';
-      },
-    });
-  });
-  iconContainerRefs.value.forEach((icon) => {
-    const initialRotate = computeRandInt(20, 180);
-    gsap.fromTo(
-      icon,
-      { rotate: initialRotate },
-      {
-        rotate: initialRotate - 360,
-        ease: 'none',
-        y: -300,
-        scrollTrigger: {
-          trigger: icon,
-          scrub: true,
-        },
-      },
-    );
-  });
-});
 </script>
 
 <template>
@@ -67,9 +32,8 @@ onMounted(() => {
         :key="index"
         class="relative max-w-6xl flex"
       >
-        <img
-          :data-src="cover_image_url"
-          :ref="(el) => imageRefs.push(el as HTMLImageElement)"
+        <ui-lazy-image
+          :src="cover_image_url"
           class="h-[300px] aspect-video object-cover bg-gray-300"
           width="500"
           height="300"
@@ -86,21 +50,13 @@ onMounted(() => {
             class="mt-10 text-sm"
           />
         </div>
-        <span
-          :ref="(el) => iconContainerRefs.push(el as HTMLSpanElement)"
-          class="absolute z-0 inline-block"
-          :style="{
-            top: `${computeRandInt(0, 50)}%`,
-            left: `${computeRandInt(0, 100)}%`,
-          }"
-        >
-          <icon-javascript v-if="index === 2" class="w-16 text-yellow-500" />
-          <icon-react v-if="index === 3" class="w-16 text-light-blue-500" />
-          <icon-typescript v-if="index === 4" class="w-16 text-blue-500" />
-          <icon-vue v-if="index === 5" class="w-16 text-green-600" />
-        </span>
+        <client-only>
+          <ui-floating-icon :index="index" />
+        </client-only>
       </li>
     </ul>
   </div>
-  <ui-contour-lines />
+  <client-only>
+    <ui-contour-lines />
+  </client-only>
 </template>
