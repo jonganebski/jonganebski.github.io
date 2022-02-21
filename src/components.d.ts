e4f9ad9df7b685c66fe788eb02bd13f28773d190
jonganebski <jon.ganebski@gmail.com>
2022-02-21 23:36:36 +0900

    :label: autogenerated

--- src/components.d.ts
+++ src/components.d.ts
@@ -4,6 +4,10 @@
 
 declare module 'vue' {
   export interface GlobalComponents {
+    CarbonCampsite: typeof import('~icons/carbon/campsite')['default']
+    CarbonCode: typeof import('~icons/carbon/code')['default']
+    CarbonLink: typeof import('~icons/carbon/link')['default']
+    ChartElevation: typeof import('./components/chart/elevation.vue')['default']
     IconGo: typeof import('./components/icon/go.vue')['default']
     IconJavascript: typeof import('./components/icon/javascript.vue')['default']
     IconReact: typeof import('./components/icon/react.vue')['default']
@@ -12,9 +16,11 @@ declare module 'vue' {
     IconVue: typeof import('./components/icon/vue.vue')['default']
     LayoutFooter: typeof import('./components/layout/footer.vue')['default']
     LayoutHeader: typeof import('./components/layout/header.vue')['default']
+    MapDetail: typeof import('./components/map/detail.vue')['default']
     MapSummary: typeof import('./components/map/summary.vue')['default']
     MdEn: typeof import('./components/md/en.vue')['default']
     MdKo: typeof import('./components/md/ko.vue')['default']
+    MdRouteVisualization: typeof import('./components/md/route-visualization.vue')['default']
     MdWrapper: typeof import('./components/md/wrapper.vue')['default']
     UiBlock: typeof import('./components/ui/block.vue')['default']
     UiCloudShadow: typeof import('./components/ui/cloud-shadow.vue')['default']
