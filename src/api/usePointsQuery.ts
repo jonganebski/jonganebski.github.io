576130a62a0336f213467374e32d1bc212b7ac7f
jonganebski <jon.ganebski@gmail.com>
2022-02-21 23:18:38 +0900

    :sparkles: create usePointsQuery

---
+++ src/api/usePointsQuery.ts
@@ -0,0 +1,51 @@
+import { LngLat } from 'mapbox-gl';
+import { useQuery } from 'vue-query';
+import type { Point, Point__point } from '~/libs/supabase';
+import { supabase } from '~/libs/supabase';
+
+export interface UsePointsQueryData extends Pick<Point, 'date' | 'points'> {}
+
+export interface UsePointsQueryResult extends Point__point {
+  distance: number;
+}
+
+const ONE_DAY = 1000 * 60 * 60 * 24;
+
+export function usePointsQuery(fileName: string) {
+  const [from, to] = fileName.split('T');
+  return useQuery(
+    ['points', fileName],
+    async () => {
+      const { data } = await supabase
+        .from<UsePointsQueryData>('points')
+        .select('date, points')
+        .gte('date', from)
+        .lte('date', to)
+        .order('date', { ascending: true });
+      if (!data) return [];
+
+      let distance = 0;
+
+      const results: UsePointsQueryResult[] = data.flatMap(({ points }) => {
+        return points.map((currentPoint, index) => {
+          const previousPoint = points[Math.max(0, index - 1)];
+          const km =
+            new LngLat(previousPoint.lon, previousPoint.lat).distanceTo(
+              new LngLat(currentPoint.lon, currentPoint.lat),
+            ) / 1000;
+          distance += km;
+          return { ...currentPoint, distance };
+        });
+      });
+
+      return results;
+    },
+    {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchOnMount: false,
+      staleTime: ONE_DAY,
+      retry: false,
+    },
+  );
+}
