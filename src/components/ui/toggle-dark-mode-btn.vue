aaa56c62b2daee71f84423c260bcfa01161d4028
jonganebski <jon.ganebski@gmail.com>
2022-02-24 22:28:08 +0900

    :sparkles: create toggle-dark-mode-btn component

---
+++ src/components/ui/toggle-dark-mode-btn.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core';
+
+const isDark = useDark();
+const toggleDarkMode = useToggle(isDark);
+</script>
+
+<template>
+  <button @click="toggleDarkMode()">
+    <carbon-moon v-if="isDark" />
+    <carbon-sun v-else />
+  </button>
+</template>
