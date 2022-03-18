61590a5604a2227331f648c6ab910c10d85f56be
jonganebski <jon.ganebski@gmail.com>
2022-03-19 00:01:52 +0900

    :sparkles: create mine-sweeper nodes

--- mine-sweeper/src/lib.rs
+++ mine-sweeper/src/lib.rs
@@ -1,6 +1,5 @@
 use std::borrow::Borrow;
-use std::cell::Cell;
-use std::rc::Rc;
+use rand::Rng;
 use wasm_bindgen::prelude::*;
 use wasm_bindgen::JsCast;
 
@@ -22,40 +21,100 @@ extern "C" {
     fn log_many(a: &str, b: &str);
 }
 
-#[wasm_bindgen]
-pub enum Level {
-    Easy = "Easy",
-    Moderate = "Moderate",
-    Hard = "Hard"
+struct Node {
+    is_veiled: bool,
+    is_mine: bool,
+    hint: i8,
 }
 
 #[wasm_bindgen]
-pub fn initialize(level: Level) -> Result<(), JsValue> {
-    log("FF");
+pub fn easy() -> Result<(), JsValue> {
+    const TARGET_MINES_COUNT:i32 = 20;
+    let mut blue_print: [[bool;8];8] = [[false;8];8];
+    let mut mines_count = 0;
+
+    while mines_count < TARGET_MINES_COUNT {
+        let col_num = rand::thread_rng().gen_range(0..8);
+        let row_num = rand::thread_rng().gen_range(0..8);
+        if blue_print[row_num][col_num] == false {
+            blue_print[row_num][col_num] = true;
+            mines_count += 1;
+        }
+    }
 
     let document = web_sys::window().unwrap().document().unwrap();
-    let size:i16;
+    
+    for row_num in 0..blue_print.len() {
+        let row = blue_print[row_num];
+        for col_num in 0..row.len() {
+            let is_mine = row[col_num];
+            let mut hint:i8= 0;
+            
+            let north = if row_num == 0 { false } else { blue_print[row_num-1][col_num] };
+            if north { hint += 1}
+            
+            let north_east = if row_num == 0 || col_num + 1 >= 8 { false } else { blue_print[row_num-1][col_num + 1] };
+            if north_east { hint += 1}
+            
+            let east = if col_num + 1 >= 8 { false } else { blue_print[row_num][col_num + 1] };
+            if east { hint += 1}
+            
+            let south_east = if col_num + 1 >= 8 || row_num + 1 >=8 { false } else { blue_print[row_num + 1][col_num + 1] };
+            if south_east { hint += 1}
+            
+            let south = if row_num + 1 >=8 { false } else { blue_print[row_num + 1][col_num] };
+            if south { hint += 1}
+            
+            let south_west = if row_num + 1 >=8 || col_num == 0 { false } else { blue_print[row_num + 1][col_num - 1] };
+            if south_west { hint += 1}
+            
+            let west = if col_num == 0 { false } else { blue_print[row_num][col_num - 1] };
+            if west { hint += 1}
+            
+            let north_west = if col_num == 0 || row_num == 0 { false } else { blue_print[row_num - 1][col_num - 1] };
+            if north_west { hint += 1}
+            
+            let node = Node {is_veiled: true, is_mine, hint};
 
-    match level {
-        Level::Easy => {size = 8 * 8},
-        Level::Moderate => {size = 16 * 16},
-        Level::Hard => {size = 16 * 30},
-        Level::__Nonexhaustive => {size = 8*8}
-    }
-    
-    
-    for number in (1..size+1).into_iter() {
-        let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
-            log(number.to_string().borrow())
-        }) as Box<dyn FnMut(_)>);
-        let div = document
+            
+            let div = document
             .create_element("div")?
             .dyn_into::<web_sys::HtmlDivElement>()?;
-        div.set_inner_text(number.to_string().borrow());
-        div.add_event_listener_with_callback("click", closure.as_ref().unchecked_ref())?;
-        closure.forget();
-        document.get_element_by_id("wasm").unwrap().append_child(&div)?;
+            
+            div.set_inner_text(node.is_mine.to_string().borrow());
+            div.set_class_name("veiled");
+            
+            let closure = Closure::wrap(Box::new(move |event: web_sys::MouseEvent| {
+                match event.current_target(){
+                    Some(current_target) => {
+                        let div = current_target.dyn_into::<web_sys::HtmlDivElement>();
+                        match div {
+                            Ok(div) => {
+                                if is_mine {
+                                    log("Game Over")
+                                }else{
+                                    div.set_inner_text(node.hint.to_string().borrow());
+                                    div.set_class_name("unveiled");
+                                }
+                            },
+                            Err(_) => ()
+                        }
+                        
+                    },
+                    None => ()
+                }
+            }) as Box<dyn FnMut(_)>);
+            
+            div.add_event_listener_with_callback("click", closure.as_ref().unchecked_ref())?;
+            
+            closure.forget();
+            
+            document.get_element_by_id("wasm").unwrap().append_child(&div)?;
+        }
     }
+
+    Ok(())
+}
 
 
 
@@ -104,6 +163,3 @@ pub fn initialize(level: Level) -> Result<(), JsValue> {
     //     canvas.add_event_listener_with_callback("mouseup", closure.as_ref().unchecked_ref())?;
     //     closure.forget();
     // }
-
-    Ok(())
-}
