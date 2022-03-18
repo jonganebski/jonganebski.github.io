4484d860af636fe7f273ca32bd0c0d8fb865b312
jonganebski <jon.ganebski@gmail.com>
2022-03-18 21:39:10 +0900

    :sparkles: create /fun page

---
+++ src/pages/fun/index.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// @ts-ignore
+import init, { initialize } from 'mine-sweeper';
+
+onMounted(() => {
+  init().then(() => {
+    initialize('Easy');
+  });
+});
+</script>
+
+<template>
+  <div class="min-h-screen flex justify-center items-center">
+    <div id="wasm"></div>
+  </div>
+</template>
+
+<style scoped lang="css">
+#wasm {
+  display: grid;
+  grid-template-columns: repeat(8, 20px);
+  grid-template-rows: repeat(8, 20px);
+  font-size: 0.5rem;
+}
+</style>
