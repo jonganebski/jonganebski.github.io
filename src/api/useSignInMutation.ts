af11a7a9457f2a81659f603a893dd45027bd0a38
jonganebski <jon.ganebski@gmail.com>
2022-03-21 22:46:29 +0900

    :sparkles: create auth apis

---
+++ src/api/useSignInMutation.ts
@@ -0,0 +1,22 @@
+import { Provider, User } from '@supabase/supabase-js';
+import { useMutation, useQueryClient } from 'vue-query';
+import { supabase } from '~/libs/supabase';
+
+type AllowedProvider = Extract<Provider, 'apple' | 'github' | 'google'>;
+
+interface UseAuthMutationVariables {
+  provider: AllowedProvider;
+}
+
+export function useSignInMutation() {
+  const queryClient = useQueryClient();
+
+  return useMutation<User | null, unknown, UseAuthMutationVariables>(
+    async ({ provider }) => (await supabase.auth.signIn({ provider })).user,
+    {
+      onSuccess: (data) => {
+        queryClient.setQueryData<User | null>('user', () => data);
+      },
+    },
+  );
+}
