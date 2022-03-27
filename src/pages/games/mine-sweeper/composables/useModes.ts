bbed63a9d29b3d9ad0636654c8e96679dc7dad9d
jonganebski <jon.ganebski@gmail.com>
2022-03-27 21:19:18 +0900

    :sparkles: create useModes

---
+++ src/pages/games/mine-sweeper/composables/useModes.ts
@@ -0,0 +1,21 @@
+import { useModesQuery } from '~/api/useModesQuery';
+
+export function useModes() {
+  const { data: modes, findModeById, isLoading } = useModesQuery('mine-sweeper-modes');
+
+  const router = useRouter();
+  const route = useRoute();
+
+  const modeId = computed<number>(() => (route.query.mode ? Number(route.query.mode) : 1));
+  const modeName = computed(() => findModeById(modeId.value));
+
+  watchEffect(() => {
+    if (isLoading.value) return;
+    if (modeName.value) return;
+    router.replace({ path: route.path });
+  });
+
+  watchEffect(() => Array.isArray(route.query.mode) && router.replace({ path: route.path }));
+
+  return { modeId, modeName, modes };
+}
