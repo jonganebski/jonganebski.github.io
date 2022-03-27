2c53d829828e34d73e9f1eb6dfb56cc364fb8ccb
jonganebski <jon.ganebski@gmail.com>
2022-03-27 18:55:53 +0900

    :sparkles: create useMineSweeperRecordsQuery

---
+++ src/api/useMineSweeperRecordsQuery.ts
@@ -0,0 +1,31 @@
+import { Ref } from 'vue';
+import { useQuery } from 'vue-query';
+import { MineSweeperRecord, Profile, supabase } from '~/libs/supabase';
+
+interface UseMineSweeperRecordsQueryData extends MineSweeperRecord {
+  user: Pick<Profile, 'user_name' | 'avatar_url'>;
+}
+
+const ONE_HOUR = 1000 * 60 * 60;
+
+export function useMineSweeperRecordsQuery(modeId: Ref<number>) {
+  return useQuery(
+    reactive(['mine-sweeper-records', modeId]),
+    async () =>
+      (
+        await supabase
+          .from<UseMineSweeperRecordsQueryData>('mine-sweeper-records')
+          .select('*, user: user_id (user_name, avatar_url)')
+          .eq('mode_id', modeId.value)
+          .order('time', { ascending: true })
+          .limit(100)
+      ).data,
+    {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      refetchOnMount: false,
+      staleTime: ONE_HOUR,
+      retry: false,
+    },
+  );
+}
