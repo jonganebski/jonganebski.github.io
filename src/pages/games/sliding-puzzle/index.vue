f211c7040d97a760a96667bec4bdc02ed1320581
jonganebski <jon.ganebski@gmail.com>
2022-03-28 12:01:33 +0900

    :lipstick: update behavior when game is done

--- src/pages/games/sliding-puzzle/index.vue
+++ src/pages/games/sliding-puzzle/index.vue
@@ -204,52 +204,57 @@ function computeBgPosition(node: number) {
     </div>
   </div>
   <div
+    class="relative grid gap-px transition-all duration-1000 ease-linear"
+    :style="{ gridTemplateRows: `auto repeat(${SIZE_Y}, ${SIZE_NODE}px) auto` }"
+  >
+    <div
+      class="absolute top-0 left-1/2 transform -translate-x-1/2 z-1 pointer-events-none"
     :style="{
-      display: 'grid',
-      gap: '1px',
-      gridTemplateRows: `auto repeat(${SIZE_Y}, ${SIZE_NODE}px) auto`,
+        width: `${SIZE_X * SIZE_NODE + SIZE_X}px`,
+        height: `${SIZE_Y * SIZE_NODE + SIZE_Y}px`,
     }"
   >
     <div
+        class="absolute top-0 left-0 z-1 w-full h-full bg-white opacity-0"
+        :class="{ 'done__anim-mask': status === 'done' }"
+      />
+      <div
+        class="w-full h-full bg-cover"
+        :class="[status === 'done' ? 'done__anim-image opacity-100' : 'opacity-0']"
+        :style="{ backgroundImage: `url(${imageUrl})` }"
+      />
+    </div>
+    <div
       v-for="(row, rowIdx) in bluePrint"
       :key="rowIdx"
-      :style="{
-        display: 'grid',
-        gridTemplateColumns: `auto repeat(${SIZE_X}, ${SIZE_NODE}px) auto`,
-        gap: '1px',
-        transition: 'all linear 1s',
-      }"
+      class="grid gap-px transition-all duration-1000 ease-linear"
+      :style="{ gridTemplateColumns: `auto repeat(${SIZE_X}, ${SIZE_NODE}px) auto` }"
     >
       <div v-for="(node, colIdx) in row" :key="node">
-        <div v-if="node === 0" :style="{ width: '0px', height: '0px' }"></div>
+        <div v-if="node === 0" class="w-0 h-0" />
         <div
           v-else-if="node === -1"
           id="void"
           :data-rowIdx="rowIdx"
           :data-colIdx="colIdx"
           :style="{ width: `${SIZE_NODE}px`, height: `${SIZE_NODE}px` }"
-        ></div>
+        />
         <button
           v-else
           :id="node.toString()"
           :data-rowIdx="rowIdx"
           :data-colIdx="colIdx"
+          class="text-white"
           :style="{
-            position: 'relative',
             width: `${SIZE_NODE}px`,
             height: `${SIZE_NODE}px`,
             backgroundImage: `url(${imageUrl})`,
             backgroundSize: `${SIZE_NODE * SIZE_X}px ${SIZE_NODE * SIZE_Y}px`,
             backgroundPosition: computeBgPosition(node),
-            color: 'white',
             transform: 'translate(0px)',
           }"
           @click="onClick($event, rowIdx, colIdx)"
         >
-          <div
-            class="absolute top-0 left-0 w-full h-full bg-white opacity-0"
-            :class="{ done: status === 'done' }"
-          ></div>
           {{ status === 'done' ? '' : node }}
         </button>
       </div>
@@ -260,7 +265,7 @@ function computeBgPosition(node: number) {
 </template>
 
 <style scoped lang="css">
-@keyframes done {
+@keyframes done-mask {
   0% {
     opacity: 0;
   }
@@ -274,8 +279,25 @@ function computeBgPosition(node: number) {
     opacity: 0;
   }
 }
+@keyframes done-image {
+  0% {
+    opacity: 0;
+  }
+  10% {
+    opacity: 0;
+  }
+  11% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 1;
+  }
+}
 
-.done {
-  animation: done linear 5s;
+.done__anim-mask {
+  animation: done-mask linear 5s;
+}
+.done__anim-image {
+  animation: done-image linear 5s;
 }
 </style>
