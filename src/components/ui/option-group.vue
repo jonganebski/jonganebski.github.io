f6be36daa60ad390b2cf5304ccc3ba86d9d77475
jonganebski <jon.ganebski@gmail.com>
2022-03-28 15:31:01 +0900

    :sparkles: create option-group component

---
+++ src/components/ui/option-group.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+interface Props {
+  label: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div>
+    <span class="pl-1">{{ props.label }}</span>
+    <div display="pl-2 grid">
+      <slot />
+    </div>
+  </div>
+</template>
