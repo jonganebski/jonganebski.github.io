9f1338855a71ac821a6493e96762a3203d4bc9be
jonganebski <jon.ganebski@gmail.com>
2022-03-28 13:46:40 +0900

    :label: add sliding-puzzle-records table interface

--- src/libs/supabase.ts
+++ src/libs/supabase.ts
@@ -124,6 +124,30 @@ export interface MineSweeperMode {
 }
 
 /*----------------------------------
+| Sliding Puzzle Records Table
+----------------------------------*/
+
+/**
+ * ### Interface of the Sliding Puzzle Records Table
+ *
+ * @example
+ * ```ts
+ * id: 1;
+ * created_at: '2022-02-07T05:56:07+00:00';
+ * updated_at: '2022-02-07T05:56:07+00:00';
+ * user_id: 'ba6546d0-1303-46ee-bddf-77b780542a91'
+ * moves_count: -2
+ * ```
+ */
+export interface SlidingPuzzleRecord {
+  id: number;
+  created_at: string;
+  updated_at: string;
+  user_id: string;
+  moves_count: number;
+}
+
+/*----------------------------------
 | Supabase Client
 ----------------------------------*/
 
