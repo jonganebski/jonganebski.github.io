749e522f34ece234eb7ebfc6b760cd47d05dfa37
jonganebski <jon.ganebski@gmail.com>
2022-03-29 23:51:47 +0900

    :lipstick: add simple styles

--- src/pages/games/index.vue
+++ src/pages/games/index.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <router-link to="/games/mine-sweeper">Mine Sweeper</router-link>
-    <router-link to="/games/sliding-puzzle">Sliding Puzzle</router-link>
+  <div class="mt-20 p-10 lg:p-20 max-w-screen-lg grid gap-3">
+    <router-link to="/games/mine-sweeper">
+      <div class="w-full px-2 py-4 shadow-lg sm:shadow-none sm:hover:shadow-lg">Mine Sweeper</div>
+    </router-link>
+    <router-link to="/games/sliding-puzzle">
+      <div class="w-full px-2 py-4 shadow-lg sm:shadow-none sm:hover:shadow-lg">Sliding Puzzle</div>
+    </router-link>
   </div>
+  <ui-contour-lines />
 </template>
