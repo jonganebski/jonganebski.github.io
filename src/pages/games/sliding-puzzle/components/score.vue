b4cb050eb4fdf153a32d52b20aced8a324ed1a0c
jonganebski <jon.ganebski@gmail.com>
2022-03-31 03:05:29 +0900

    :boom: update score component

--- src/pages/games/sliding-puzzle/components/score.vue
+++ src/pages/games/sliding-puzzle/components/score.vue
@@ -1,26 +1,54 @@
 <script setup lang="ts">
+import { useMyI18n } from '~/plugins/i18n';
+
 interface Props {
+  clicksCount: number;
   shuffleCount: number;
-  clickCount: number;
-  score: number;
+  score: number | null;
+  time: number | null;
 }
 
 const props = defineProps<Props>();
+
+const { t } = useMyI18n();
+
+const duration = computed(() => {
+  if (!props.time) return null;
+  const minutes = Math.floor(props.time / (1000 * 60));
+  const seconds = Math.floor((props.time - minutes * 1000 * 60) / 1000);
+  const milliseconds = props.time - minutes * 1000 * 60 - seconds * 1000;
+  return { minutes, seconds, milliseconds };
+});
 </script>
 
 <template>
-  <div>
-    <div>{{ props.clickCount }}</div>
-    <div
-      :class="[
-        props.score < 0
-          ? 'text-green-500'
-          : props.score < props.shuffleCount
-          ? 'text-yellow-500'
-          : 'text-red-500',
-      ]"
-    >
-      {{ props.score > 0 ? `+${props.score}` : props.score }} moves
-    </div>
+  <div class="text-sm">
+    <span>
+      {{ t('move', 2) }}:
+      <span
+        :class="[
+          props.clicksCount <= props.shuffleCount
+            ? 'text-green-500'
+            : props.clicksCount < props.shuffleCount * 2
+            ? 'text-yellow-500'
+            : 'text-red-500',
+        ]"
+      >
+        {{ props.clicksCount }}
+      </span>
+    </span>
+    <p v-if="duration" class="">
+      <span>{{ t('time') }}: </span>
+      <span v-if="duration.minutes">
+        {{ duration.minutes }} {{ t('minutes').toLowerCase() }} {{ ' ' }}
+      </span>
+      <span v-if="duration.seconds">
+        {{ duration.seconds }} {{ t('seconds').toLowerCase() }} {{ ' ' }}
+      </span>
+      <span v-if="duration.milliseconds">
+        {{ duration.milliseconds }} {{ t('milliseconds').toLowerCase() }}
+      </span>
+    </p>
+    <p v-if="score">{{ t('score') }}: {{ score }}</p>
   </div>
 </template>
