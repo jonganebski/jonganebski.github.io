048e323667b111538d1046b328b1931e4cb82ef2
jonganebski <jon.ganebski@gmail.com>
2022-03-31 15:14:29 +0900

    :wheelchair: add aria-label

--- src/components/ui/toggle-dark-mode-btn.vue
+++ src/components/ui/toggle-dark-mode-btn.vue
@@ -1,12 +1,20 @@
 <script setup lang="ts">
 import { useDark, useToggle } from '@vueuse/core';
+import { useMyI18n } from '~/plugins/i18n';
+
+const { t } = useMyI18n();
 
 const isDark = useDark();
 const toggleDarkMode = useToggle(isDark);
 </script>
 
 <template>
-  <button @click="toggleDarkMode()">
+  <button
+    @click="toggleDarkMode()"
+    :aria-label="
+      isDark ? t('click_to_change_to_the_light_mode') : t('click_to_change_to_the_dark_mode')
+    "
+  >
     <carbon-moon v-if="isDark" />
     <carbon-sun v-else />
   </button>
