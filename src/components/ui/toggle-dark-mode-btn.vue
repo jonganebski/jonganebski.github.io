ac8ac01d0c756accdca2eedf03627cd508e57bbc
Jon Ganebski <jon.ganebski@gmail.com>
2022-04-01 21:35:02 +0900

    :sparkles: create useDarkMode

--- src/components/ui/toggle-dark-mode-btn.vue
+++ src/components/ui/toggle-dark-mode-btn.vue
@@ -1,19 +1,16 @@
 <script setup lang="ts">
-import { useDark, useToggle } from '@vueuse/core';
+import { isDark, toggleDarkMode } from '~/composables/useDarkMode';
 import { useMyI18n } from '~/plugins/i18n';
 
 const { t } = useMyI18n();
-
-const isDark = useDark();
-const toggleDarkMode = useToggle(isDark);
 </script>
 
 <template>
   <button
-    @click="toggleDarkMode()"
     :aria-label="
       isDark ? t('click_to_change_to_the_light_mode') : t('click_to_change_to_the_dark_mode')
     "
+    @click="toggleDarkMode()"
   >
     <carbon-moon v-if="isDark" />
     <carbon-sun v-else />
