5a671d0ef6425c71f2cb15c59e6a02db01099056
Jon Ganebski <jon.ganebski@gmail.com>
2022-04-01 17:16:40 +0900

    :lipstick: update style /games page

--- src/pages/games/index.vue
+++ src/pages/games/index.vue
@@ -1,13 +1,55 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useMyI18n } from '~/plugins/i18n';
+
+const { t, locale } = useMyI18n();
+
+const STORAGE_URL = 'https://ijivzwfsihdcvwrntdpe.supabase.co/storage/v1/object/public/game-images';
+
+const links = computed(() => [
+  {
+    path: '/games/mine-sweeper',
+    name: t('minesweeper'),
+    src: `${STORAGE_URL}/screenshot-minesweeper.jpg`,
+  },
+  {
+    path: '/games/sliding-puzzle',
+    name: t('sliding_puzzle'),
+    src: `${STORAGE_URL}/screenshot-sliding-puzzle.jpg`,
+  },
+]);
+</script>
 
 <template>
-  <div class="mt-20 p-10 lg:p-20 max-w-screen-lg grid gap-3">
-    <router-link to="/games/mine-sweeper">
-      <div class="w-full px-2 py-4 shadow-lg sm:shadow-none sm:hover:shadow-lg">Mine Sweeper</div>
-    </router-link>
-    <router-link to="/games/sliding-puzzle">
-      <div class="w-full px-2 py-4 shadow-lg sm:shadow-none sm:hover:shadow-lg">Sliding Puzzle</div>
-    </router-link>
-  </div>
-  <ui-contour-lines />
+  <ul class="mt-20 mx-auto p-10 lg:p-20 max-w-screen-xl flex flex-wrap gap-16">
+    <li v-for="{ path, name, src } in links" :key="path">
+      <router-link
+        class="group"
+        :to="path"
+        :aria-label="
+          locale === 'ko' ? `${name}을(를) 플레이하려면 클릭하세요` : `Click to play ${name}`
+        "
+      >
+        <ui-lazy-image
+          class="transition-shadow shadow-sm group-hover:shadow-xl"
+          :style="{ transitionDelay: `${name.length * 10}ms` }"
+          :height="240"
+          :width="200"
+          :src="src"
+        />
+        <h4 class="mt-2 text-center overflow-hidden">
+          <span
+            v-for="(char, index) in name.split('')"
+            :key="index"
+            class="inline-block transition-all transform translate-y-[1em] group-hover:translate-y-0 opacity-0 group-hover:opacity-100"
+            :style="{ transitionDelay: `${index * 10}ms` }"
+          >
+            {{ char }}
+          </span>
+        </h4>
+      </router-link>
+    </li>
+  </ul>
+  <client-only>
+    <ui-contour-lines />
+  </client-only>
 </template>
