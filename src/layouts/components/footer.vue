ec87af9fdec64f82a5eee1412fbc7f22002145af
Jon Ganebski <jon.ganebski@gmail.com>
2022-04-01 17:36:25 +0900

    :sparkles: add games links on the footer

--- src/layouts/components/footer.vue
+++ src/layouts/components/footer.vue
@@ -9,13 +9,18 @@ const routePostsRecent = routePosts?.slice(-5).reverse();
 
 const techPosts = getTechPosts();
 const techPostsRecent = techPosts?.sort((a, b) => b.date.localeCompare(a.date)).slice(-5);
+
+const gameLinks = computed(() => [
+  { path: '/games/mine-sweeper', name: t('minesweeper') },
+  { path: '/games/sliding-puzzle', name: t('sliding_puzzle') },
+]);
 </script>
 
 <template>
   <footer
     class="min-h-[32rem] mt-32 pt-32 pb-5 px-4 md:px-28 flex flex-col justify-between bg-transparent text-dark-500 dark:text-light-500"
   >
-    <div class="w-full max-w-screen-lg mx-auto grid sm:grid-cols-2 lg:grid-cols-3 gap-10">
+    <div class="w-full max-w-screen-lg mx-auto grid sm:grid-cols-2 lg:grid-cols-4 gap-10">
       <div>
         <h6><carbon-campsite class="icon" /></h6>
         <nav class="nav">
@@ -39,6 +44,19 @@ const techPostsRecent = techPosts?.sort((a, b) => b.date.localeCompare(a.date)).
             class="recent-post-link"
           >
             {{ title[locale] }}
+          </router-link>
+        </nav>
+      </div>
+      <div>
+        <h6><carbon-game-console class="icon" /></h6>
+        <nav class="nav">
+          <router-link
+            v-for="{ name, path } in gameLinks"
+            :key="path"
+            :to="path"
+            class="recent-post-link"
+          >
+            {{ name }}
           </router-link>
         </nav>
       </div>
