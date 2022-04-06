b76b7d2c2ff2b0f8cb85a8f919e89c2211174e07
Jon Ganebski <jon.ganebski@gmail.com>
2022-04-06 18:51:28 +0900

    :alembic: perform experiments on tetris

---
+++ src/pages/games/tetris/composables/useI.ts
@@ -0,0 +1,129 @@
+import { useNodes } from './useNodes';
+
+export function useI() {
+  const { nodes, NODE, TOP_RESERVE } = useNodes();
+  const defaultPosition = Object.freeze([
+    [0, 4],
+    [1, 4],
+    [2, 4],
+    [3, 4],
+  ]);
+
+  const position = ref([...defaultPosition]);
+  const shape = ref<0 | 1>(0);
+  const nextPosition = computed(() =>
+    position.value.map(([rowIdx, colIdx]) => [rowIdx + 1, colIdx]),
+  );
+
+  function prepare() {
+    shape.value = 0;
+    position.value = [...defaultPosition];
+    position.value.forEach(([rowIdx, colIdx]) => (nodes.value[rowIdx][colIdx] = NODE.I));
+  }
+
+  function fall() {
+    let isBlocked = false;
+    for (let i = 0; i < nextPosition.value.length; i++) {
+      const [nextRowIdx, nextColIdx] = nextPosition.value[i];
+      if (
+        nodes.value[nextRowIdx][nextColIdx] === NODE.BIRTH ||
+        nodes.value[nextRowIdx][nextColIdx] === NODE.VOID ||
+        nodes.value[nextRowIdx][nextColIdx] === NODE.I
+      )
+        continue;
+      isBlocked = true;
+      break;
+    }
+    if (isBlocked) {
+      position.value.forEach(([rowIdx, colIdx]) => (nodes.value[rowIdx][colIdx] = NODE.FOSSIL_I));
+      return false;
+    }
+    position.value.forEach(([rowIdx, colIdx]) => {
+      nodes.value[rowIdx][colIdx] = rowIdx <= TOP_RESERVE - 1 ? NODE.BIRTH : NODE.VOID;
+    });
+    nextPosition.value.forEach(([nextRowIdx, nextColIdx]) => {
+      nodes.value[nextRowIdx][nextColIdx] = NODE.I;
+    });
+
+    position.value = nextPosition.value;
+    return true;
+  }
+
+  function moveRight() {
+    let willCollide = false;
+    position.value.forEach(([rowIdx, colIdx]) => {
+      if (
+        nodes.value[rowIdx][colIdx + 1] !== NODE.VOID &&
+        nodes.value[rowIdx][colIdx + 1] !== NODE.BIRTH &&
+        nodes.value[rowIdx][colIdx + 1] !== NODE.I
+      )
+        willCollide = true;
+    });
+    if (willCollide) return;
+    position.value.forEach(([rowIdx, colIdx], i) => {
+      nodes.value[rowIdx][colIdx] = rowIdx <= TOP_RESERVE - 1 ? NODE.BIRTH : NODE.VOID;
+      position.value[i] = [rowIdx, colIdx + 1];
+    });
+    position.value.forEach(([rowIdx, colIdx]) => {
+      nodes.value[rowIdx][colIdx] = NODE.I;
+    });
+  }
+
+  function moveLeft() {
+    let willCollide = false;
+    position.value.forEach(([rowIdx, colIdx]) => {
+      if (
+        nodes.value[rowIdx][colIdx - 1] !== NODE.VOID &&
+        nodes.value[rowIdx][colIdx - 1] !== NODE.BIRTH &&
+        nodes.value[rowIdx][colIdx - 1] !== NODE.I
+      )
+        willCollide = true;
+    });
+    if (willCollide) return;
+    position.value.forEach(([rowIdx, colIdx], i) => {
+      nodes.value[rowIdx][colIdx] = rowIdx <= TOP_RESERVE - 1 ? NODE.BIRTH : NODE.VOID;
+      position.value[i] = [rowIdx, colIdx - 1];
+    });
+    position.value.forEach(([rowIdx, colIdx]) => {
+      nodes.value[rowIdx][colIdx] = NODE.I;
+    });
+  }
+
+  function changeShape() {
+    position.value.forEach(([rowIdx, colIdx]) => {
+      nodes.value[rowIdx][colIdx] = rowIdx <= TOP_RESERVE - 1 ? NODE.BIRTH : NODE.VOID;
+    });
+    if (shape.value === 0) {
+      position.value.forEach(([rowIdx, colIdx], i, arr) => {
+        if (i === 0) position.value[i] = [arr[2][0], arr[2][1] - 2];
+        if (i === 1) position.value[i] = [arr[2][0], arr[2][1] - 1];
+        if (i === 2) position.value[i] = [rowIdx, colIdx];
+        if (i === 3) position.value[i] = [arr[2][0], arr[2][1] + 1];
+      });
+      shape.value = 1;
+    } else {
+      position.value.forEach(([rowIdx, colIdx], i, arr) => {
+        if (i === 0) position.value[i] = [arr[2][0] - 2, arr[2][1]];
+        if (i === 1) position.value[i] = [arr[2][0] - 1, arr[2][1]];
+        if (i === 2) position.value[i] = [rowIdx, colIdx];
+        if (i === 3) position.value[i] = [arr[2][0] + 1, arr[2][1]];
+      });
+      shape.value = 0;
+    }
+    position.value.forEach(([rowIdx, colIdx]) => {
+      nodes.value[rowIdx][colIdx] = NODE.I;
+    });
+  }
+
+  return {
+    defaultPosition,
+    nextPosition,
+    position,
+    shape,
+    prepare,
+    fall,
+    moveRight,
+    moveLeft,
+    changeShape,
+  };
+}
--- src/pages/games/tetris/index.vue
+++ src/pages/games/tetris/index.vue
@@ -1,170 +1,66 @@
 <script setup lang="ts">
 import { onKeyStroke } from '@vueuse/core';
+import { useI } from './composables/useI';
+import { useNodes } from './composables/useNodes';
+import { useO } from './composables/useO';
 
-enum NODE {
-  WALL = 0,
-  BIRTH,
-  VOID,
-  I,
-  J,
-  L,
-  O,
-  S,
-  T,
-  Z,
-  FOSSIL_I,
-  FOSSIL_J,
-  FOSSIL_L,
-  FOSSIL_O,
-  FOSSIL_S,
-  FOSSIL_T,
-  FOSSIL_Z,
-}
+const { nodes, NODE, NODE_SIZE, currNode, nextNode, switchNode, X_SIZE, Y_SIZE, TOP_RESERVE } =
+  useNodes();
 
-const NODE_SIZE = 30;
-const X_SIZE = 9;
-const Y_SIZE = 15;
-const TOP_RESERVE = 4;
+const I = useI();
+const O = useO();
 
-const nodes = ref<NODE[][]>(generateCleanTemplate());
+const defaultSetTimeoutMs = 200;
+let setTimeoutId: NodeJS.Timeout;
+const setTimeoutMs = ref(defaultSetTimeoutMs);
 
-function generateCleanTemplate(): NODE[][] {
-  return Array.from({ length: Y_SIZE }, (_, i) =>
-    i === Y_SIZE - 1
-      ? Array.from({ length: X_SIZE }, () => NODE.WALL)
-      : Array.from({ length: X_SIZE }, (_, j) =>
-          j === 0 || j === X_SIZE - 1 ? NODE.WALL : i <= TOP_RESERVE - 1 ? NODE.BIRTH : NODE.VOID,
-        ),
-  );
-}
-const defaultPositionI = Object.freeze([
-  [0, 4],
-  [1, 4],
-  [2, 4],
-  [3, 4],
-]);
-const positionI = ref([...defaultPositionI]);
-const shape = ref<0 | 1>(0);
-const nextPositionI = computed(() =>
-  positionI.value.map(([rowIdx, colIdx]) => [rowIdx + 1, colIdx]),
-);
-
-function prepare() {
-  shape.value = 0;
-  positionI.value = [...defaultPositionI];
-  positionI.value.forEach(([rowIdx, colIdx]) => (nodes.value[rowIdx][colIdx] = NODE.I));
+function startGame() {
+  currNode.value === NODE.I ? I.prepare() : currNode.value === NODE.O ? O.prepare() : null;
+  fall();
 }
 
 function fall() {
-  let isBlocked = false;
-  for (let i = 0; i < nextPositionI.value.length; i++) {
-    const [nextRowIdx, nextColIdx] = nextPositionI.value[i];
-    if (
-      nodes.value[nextRowIdx][nextColIdx] === NODE.BIRTH ||
-      nodes.value[nextRowIdx][nextColIdx] === NODE.VOID ||
-      nodes.value[nextRowIdx][nextColIdx] === NODE.I
-    )
-      continue;
-    isBlocked = true;
-    break;
-  }
-  if (isBlocked) {
-    positionI.value.forEach(([rowIdx, colIdx]) => (nodes.value[rowIdx][colIdx] = NODE.FOSSIL_I));
-    return false;
-  }
-  positionI.value.forEach(([rowIdx, colIdx]) => {
-    nodes.value[rowIdx][colIdx] = rowIdx <= TOP_RESERVE - 1 ? NODE.BIRTH : NODE.VOID;
-  });
-  nextPositionI.value.forEach(([nextRowIdx, nextColIdx]) => {
-    nodes.value[nextRowIdx][nextColIdx] = NODE.I;
-  });
-
-  positionI.value = nextPositionI.value;
-  return true;
-}
-
-let setTimeoutId: NodeJS.Timeout;
-const setTimeoutMs = ref(500);
-
-function startGame() {
-  prepare();
-  startFalling();
-}
-
-function startFalling() {
   clearTimeout(setTimeoutId);
   setTimeoutId = setTimeout(() => {
-    if (!fall()) {
+    if (currNode.value === NODE.I ? !I.fall() : !O.fall()) {
       clearTimeout(setTimeoutId);
-      prepare();
+      if (!validate()) {
+        window.alert('Game Over!');
+        return;
+      }
+      switchNode();
+      currNode.value === NODE.I ? I.prepare() : O.prepare();
     }
-    startFalling();
+    fall();
   }, setTimeoutMs.value);
+}
+
+function validate() {
+  const target = currNode.value === NODE.I ? I.position.value : O.position.value;
+  for (let i = 0; i < target.length; i++) {
+    const [rowIdx] = target[i];
+    if (rowIdx < TOP_RESERVE) return false;
+  }
+  return true;
 }
 
 onKeyStroke('ArrowUp', (e) => {
   e.preventDefault();
-  positionI.value.forEach(([rowIdx, colIdx]) => {
-    nodes.value[rowIdx][colIdx] = rowIdx <= TOP_RESERVE - 1 ? NODE.BIRTH : NODE.VOID;
-  });
-  if (shape.value === 0) {
-    positionI.value.forEach(([rowIdx, colIdx], i, arr) => {
-      if (i === 0) positionI.value[i] = [arr[2][0], arr[2][1] - 2];
-      if (i === 1) positionI.value[i] = [arr[2][0], arr[2][1] - 1];
-      if (i === 2) positionI.value[i] = [rowIdx, colIdx];
-      if (i === 3) positionI.value[i] = [arr[2][0], arr[2][1] + 1];
-    });
-    shape.value = 1;
-  } else {
-    positionI.value.forEach(([rowIdx, colIdx], i, arr) => {
-      if (i === 0) positionI.value[i] = [arr[2][0] - 2, arr[2][1]];
-      if (i === 1) positionI.value[i] = [arr[2][0] - 1, arr[2][1]];
-      if (i === 2) positionI.value[i] = [rowIdx, colIdx];
-      if (i === 3) positionI.value[i] = [arr[2][0] + 1, arr[2][1]];
-    });
-    shape.value = 0;
-  }
-  positionI.value.forEach(([rowIdx, colIdx]) => {
-    nodes.value[rowIdx][colIdx] = NODE.I;
-  });
+  currNode.value === NODE.I ? I.changeShape() : null;
 });
 onKeyStroke('ArrowRight', (e) => {
   e.preventDefault();
-  let willCollide = false;
-  positionI.value.forEach(([rowIdx, colIdx], i) => {
-    if (
-      nodes.value[rowIdx][colIdx + 1] !== NODE.VOID &&
-      nodes.value[rowIdx][colIdx + 1] !== NODE.BIRTH
-    )
-      willCollide = true;
-  });
-  if (willCollide) return;
-  positionI.value.forEach(([rowIdx, colIdx], i) => {
-    nodes.value[rowIdx][colIdx] = rowIdx <= TOP_RESERVE - 1 ? NODE.BIRTH : NODE.VOID;
-    positionI.value[i] = [rowIdx, colIdx + 1];
-  });
-  positionI.value.forEach(([rowIdx, colIdx]) => {
-    nodes.value[rowIdx][colIdx] = NODE.I;
-  });
+  currNode.value === NODE.I ? I.moveRight() : O.moveRight();
 });
 onKeyStroke('ArrowLeft', (e) => {
   e.preventDefault();
-  let willCollide = false;
-  positionI.value.forEach(([rowIdx, colIdx], i) => {
-    if (
-      nodes.value[rowIdx][colIdx - 1] !== NODE.VOID &&
-      nodes.value[rowIdx][colIdx - 1] !== NODE.BIRTH
-    )
-      willCollide = true;
-  });
-  if (willCollide) return;
-  positionI.value.forEach(([rowIdx, colIdx], i) => {
-    nodes.value[rowIdx][colIdx] = rowIdx <= TOP_RESERVE - 1 ? NODE.BIRTH : NODE.VOID;
-    positionI.value[i] = [rowIdx, colIdx - 1];
-  });
-  positionI.value.forEach(([rowIdx, colIdx]) => {
-    nodes.value[rowIdx][colIdx] = NODE.I;
-  });
+  currNode.value === NODE.I ? I.moveLeft() : O.moveLeft();
+});
+onKeyStroke('ArrowDown', (e) => {
+  e.preventDefault();
+  setTimeoutMs.value = 0;
+  fall();
+  setTimeoutMs.value = defaultSetTimeoutMs;
 });
 
 // onRenderTriggered(() => {
@@ -175,18 +71,25 @@ onKeyStroke('ArrowLeft', (e) => {
 <template>
   <div>Tetris</div>
   <button @click="startGame">Start</button>
-  <button @click="fall">Fall</button>
+  <button @click="I.fall">Fall</button>
+  <div>
+    <p>Current Node: {{ currNode }}</p>
+    <p>Next Node: {{ nextNode }}</p>
+  </div>
   <div class="flex justify-center items-center">
     <div
       class="grid gap-px bg-black"
-      :style="{ width: `${NODE_SIZE * 9 + 8}px`, height: `${NODE_SIZE * 15 + 14}px` }"
+      :style="{ width: `${NODE_SIZE * X_SIZE + 8}px`, height: `${NODE_SIZE * Y_SIZE + 14}px` }"
     >
       <div v-for="(raw, index) in nodes" :key="index" class="grid grid-cols-9 gap-px bg-black">
         <div
           v-for="(node, index) in raw"
           :key="index"
           class="bg-white"
-          :class="{ 'bg-blue-200': node === NODE.I || node === NODE.FOSSIL_I }"
+          :class="{
+            'bg-blue-200': node === NODE.I || node === NODE.FOSSIL_I,
+            'bg-red-200': node === NODE.O || node === NODE.FOSSIL_O,
+          }"
           :style="{ width: `${NODE_SIZE}px`, height: `${NODE_SIZE}px` }"
         >
           {{ node }}
