a155a4ec85c532d6954e23499a0f4c350f11c928
Jon Ganebski <jon.ganebski@gmail.com>
2022-04-08 22:49:00 +0900

    :lipstick: update styles

--- src/pages/games/tetris/components/next-tetromino.vue
+++ src/pages/games/tetris/components/next-tetromino.vue
@@ -1,59 +1,76 @@
 <script setup lang="ts">
 import { NODE } from '../composables/@types';
 import { useNodes } from '../composables/useNodes';
-import { useStyles } from '../composables/useStyles';
 
 const { nextNode } = useNodes();
-
-const { bg } = useStyles();
 </script>
 
 <template>
-  <div class="w-28 h-28 flex items-center justify-center border border-gray-400 rounded-md">
+  <div
+    class="w-20 md:w-28 h-20 md:h-28 flex items-center justify-center bg-gray-900 border border-gray-600 rounded-md"
+  >
     <div v-if="nextNode === NODE.I" class="grid gap-px">
-      <div v-for="num in 4" :key="num" class="w-6 h-6" :class="[bg.I]" />
+      <div
+        v-for="num in 4"
+        :key="num"
+        class="w-3 md:w-4 h-3 md:h-4 border border-[0.375rem] md:border-[0.5rem] tetromino__I"
+      />
     </div>
     <div v-if="nextNode === NODE.J" class="grid gap-px grid-cols-2">
       <div
         v-for="num in 6"
         :key="num"
-        class="w-6 h-6"
-        :class="[num === 5 || num % 2 === 0 ? bg.J : '']"
+        class="w-3 md:w-4 h-3 md:h-4"
+        :class="{
+          'border border-[0.375rem] md:border-[0.5rem] tetromino__J': num === 5 || num % 2 === 0,
+        }"
       />
     </div>
     <div v-if="nextNode === NODE.L" class="grid gap-px grid-cols-2">
       <div
         v-for="num in 6"
         :key="num"
-        class="w-6 h-6"
-        :class="[num === 6 || num % 2 === 1 ? bg.L : '']"
+        class="w-3 md:w-4 h-3 md:h-4"
+        :class="{
+          'border border-[0.375rem] md:border-[0.5rem] tetromino__L': num === 6 || num % 2 === 1,
+        }"
       />
     </div>
     <div v-if="nextNode === NODE.O" class="grid gap-px grid-cols-2">
-      <div v-for="num in 4" :key="num" class="w-6 h-6" :class="[bg.O]" />
+      <div
+        v-for="num in 4"
+        :key="num"
+        class="w-3 md:w-4 h-3 md:h-4 border border-[0.375rem] md:border-[0.5rem] tetromino__O"
+      />
     </div>
     <div v-if="nextNode === NODE.S" class="grid gap-px grid-cols-3">
       <div
         v-for="num in 6"
         :key="num"
-        class="w-6 h-6"
-        :class="[[2, 3, 4, 5].includes(num) ? bg.S : '']"
+        class="w-3 md:w-4 h-3 md:h-4"
+        :class="{
+          'border border-[0.375rem] md:border-[0.5rem] tetromino__S': [2, 3, 4, 5].includes(num),
+        }"
       />
     </div>
     <div v-if="nextNode === NODE.T" class="grid gap-px grid-cols-3">
       <div
         v-for="num in 6"
         :key="num"
-        class="w-6 h-6"
-        :class="[[2, 4, 5, 6].includes(num) ? bg.T : '']"
+        class="w-3 md:w-4 h-3 md:h-4"
+        :class="{
+          'border border-[0.375rem] md:border-[0.5rem] tetromino__T': [2, 4, 5, 6].includes(num),
+        }"
       />
     </div>
     <div v-if="nextNode === NODE.Z" class="grid gap-px grid-cols-3">
       <div
         v-for="num in 6"
         :key="num"
-        class="w-6 h-6"
-        :class="[[1, 2, 5, 6].includes(num) ? bg.Z : '']"
+        class="w-3 md:w-4 h-3 md:h-4"
+        :class="{
+          'border border-[0.375rem] md:border-[0.5rem] tetromino__Z': [1, 2, 5, 6].includes(num),
+        }"
       />
     </div>
   </div>
--- src/pages/games/tetris/index.vue
+++ src/pages/games/tetris/index.vue
@@ -5,7 +5,6 @@ import { NODE } from './composables/@types';
 import { useController } from './composables/useController';
 import { useGameInfo } from './composables/useGameInfo';
 import { useNodes } from './composables/useNodes';
-import { useStyles } from './composables/useStyles';
 
 const { nodes, TOP_RESERVE, X_SIZE } = useNodes();
 
@@ -21,8 +20,6 @@ const {
 
 const { score, level } = useGameInfo();
 
-const { bg } = useStyles();
-
 onKeyStroke('ArrowUp', rotateTetromino);
 onKeyStroke('ArrowRight', moveTetrominoToRight);
 onKeyStroke('ArrowLeft', moveTetrominoToLeft);
@@ -33,13 +30,13 @@ onKeyStroke(' ', dropTetromino);
 <template>
   <h1 class="my-20 text-3xl md:text-5xl text-center">Tetris</h1>
   <br />
-  <div class="flex justify-center items-start gap-20">
-    <div class="grid gap-px shadow-lg">
+  <div class="flex justify-center items-start gap-3 lg:gap-20">
+    <div class="grid gap-px shadow-lg bg-gray-700">
       <div
         v-for="(raw, rowIdx) in nodes"
         :key="rowIdx"
         :style="{
-          display: 'grid',
+          display: rowIdx < TOP_RESERVE ? 'none' : 'grid',
           gap: '1px',
           gridTemplateColumns: `repeat(${X_SIZE - 2}, 1fr)`,
         }"
@@ -47,24 +44,29 @@ onKeyStroke(' ', dropTetromino);
         <div
           v-for="(node, colIdx) in raw"
           :key="colIdx"
-          class="bg-gray-400"
+          class="bg-gray-900"
           :class="[
-            node === NODE.WALL || rowIdx < TOP_RESERVE ? 'w-0 h-0 hidden' : 'w-8 h-8',
-            isGuide(rowIdx, colIdx) ? 'border border-red-500' : '',
-            node === NODE.I || node === NODE.FOSSIL_I
-              ? bg.I
+            node === NODE.WALL || rowIdx < TOP_RESERVE
+              ? 'w-0 h-0 hidden'
+              : 'w-5 sm:w-6 md:w-8 h-5 sm:h-6 md:h-8',
+            node === NODE.VOID
+              ? isGuide(rowIdx, colIdx)
+                ? 'guide border'
+                : ''
+              : node === NODE.I || node === NODE.FOSSIL_I
+              ? 'tetromino__I border border-[0.625rem] md:border-[1rem]'
               : node === NODE.J || node === NODE.FOSSIL_J
-              ? bg.J
+              ? 'tetromino__J border border-[0.625rem] md:border-[1rem]'
               : node === NODE.L || node === NODE.FOSSIL_L
-              ? bg.L
+              ? 'tetromino__L border border-[0.625rem] md:border-[1rem]'
               : node === NODE.O || node === NODE.FOSSIL_O
-              ? bg.O
+              ? 'tetromino__O border border-[0.625rem] md:border-[1rem]'
               : node === NODE.S || node === NODE.FOSSIL_S
-              ? bg.S
+              ? 'tetromino__S border border-[0.625rem] md:border-[1rem]'
               : node === NODE.T || node === NODE.FOSSIL_T
-              ? bg.T
+              ? 'tetromino__T border border-[0.625rem] md:border-[1rem]'
               : node === NODE.Z || node === NODE.FOSSIL_Z
-              ? bg.Z
+              ? 'tetromino__Z border border-[0.625rem] md:border-[1rem]'
               : '',
           ]"
         ></div>
@@ -81,3 +83,60 @@ onKeyStroke(' ', dropTetromino);
     <ui-contour-lines class="opacity-50" />
   </client-only>
 </template>
+
+<style scoped lang="css">
+.guide {
+  @apply border-t-red-500 border-r-red-500 border-b-red-500 border-l-red-500;
+}
+</style>
+
+<style lang="css">
+.tetromino__I {
+  border-top-color: rgba(120, 239, 255, 1);
+  border-right-color: rgba(120, 239, 255, 0.8);
+  border-bottom-color: rgba(120, 239, 255, 0.6);
+  border-left-color: rgba(120, 239, 255, 0.4);
+}
+
+.tetromino__J {
+  border-top-color: rgba(25, 154, 255, 1);
+  border-right-color: rgba(25, 154, 255, 0.8);
+  border-bottom-color: rgba(25, 154, 255, 0.6);
+  border-left-color: rgba(25, 154, 255, 0.4);
+}
+
+.tetromino__L {
+  border-top-color: rgba(82, 230, 73, 1);
+  border-right-color: rgba(82, 230, 73, 0.8);
+  border-bottom-color: rgba(82, 230, 73, 0.6);
+  border-left-color: rgba(82, 230, 73, 0.4);
+}
+
+.tetromino__O {
+  border-top-color: rgba(255, 13, 19, 1);
+  border-right-color: rgba(255, 13, 19, 0.8);
+  border-bottom-color: rgba(255, 13, 19, 0.6);
+  border-left-color: rgba(255, 13, 19, 0.4);
+}
+
+.tetromino__S {
+  border-top-color: rgba(230, 106, 226, 1);
+  border-right-color: rgba(230, 106, 226, 0.8);
+  border-bottom-color: rgba(230, 106, 226, 0.6);
+  border-left-color: rgba(230, 106, 226, 0.4);
+}
+
+.tetromino__T {
+  border-top-color: rgba(255, 232, 3, 1);
+  border-right-color: rgba(255, 232, 3, 0.8);
+  border-bottom-color: rgba(255, 232, 3, 0.6);
+  border-left-color: rgba(255, 232, 3, 0.4);
+}
+
+.tetromino__Z {
+  border-top-color: rgba(255, 154, 0, 1);
+  border-right-color: rgba(255, 154, 0, 0.8);
+  border-bottom-color: rgba(255, 154, 0, 0.6);
+  border-left-color: rgba(255, 154, 0, 0.4);
+}
+</style>
