bf9f6ad968d19d95a79d574f6f49b405f144ee00
Jon Ganebski <jon.ganebski@gmail.com>
2022-04-08 11:36:13 +0900

    :sparkles: create useJ

--- src/pages/games/tetris/index.vue
+++ src/pages/games/tetris/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onKeyStroke } from '@vueuse/core';
 import { useI } from './composables/useI';
+import { useJ } from './composables/useJ';
 import { useNodes } from './composables/useNodes';
 import { useO } from './composables/useO';
 
@@ -8,35 +9,42 @@ const { nodes, NODE, NODE_SIZE, currNode, nextNode, switchNode, X_SIZE, Y_SIZE, 
   useNodes();
 
 const I = useI();
+const J = useJ();
 const O = useO();
 
 const defaultSetTimeoutMs = 700;
 let setTimeoutId: NodeJS.Timeout;
 const setTimeoutMs = ref(defaultSetTimeoutMs);
 
+function tetromino() {
+  const t = [O, I, J].find(({ id }) => id === currNode.value);
+  if (!t) throw Error('Tetromino not found');
+  return t;
+}
+
 function startGame() {
-  currNode.value === NODE.I ? I.prepare() : currNode.value === NODE.O ? O.prepare() : null;
+  tetromino().prepare();
   fall();
 }
 
 function fall() {
   clearTimeout(setTimeoutId);
   setTimeoutId = setTimeout(() => {
-    if (currNode.value === NODE.I ? !I.fall() : !O.fall()) {
+    if (!tetromino().fall()) {
       clearTimeout(setTimeoutId);
       if (!validate()) {
         window.alert('Game Over!');
         return;
       }
       switchNode();
-      currNode.value === NODE.I ? I.prepare() : O.prepare();
+      tetromino().prepare();
     }
     fall();
   }, setTimeoutMs.value);
 }
 
 function validate() {
-  const target = currNode.value === NODE.I ? I.position.value : O.position.value;
+  const target = tetromino().position.value;
   for (let i = 0; i < target.length; i++) {
     const [rowIdx] = target[i];
     if (rowIdx < TOP_RESERVE) return false;
@@ -46,15 +54,15 @@ function validate() {
 
 onKeyStroke('ArrowUp', (e) => {
   e.preventDefault();
-  currNode.value === NODE.I ? I.changeShape() : null;
+  tetromino().changeShape();
 });
 onKeyStroke('ArrowRight', (e) => {
   e.preventDefault();
-  currNode.value === NODE.I ? I.moveRight() : O.moveRight();
+  tetromino().moveRight();
 });
 onKeyStroke('ArrowLeft', (e) => {
   e.preventDefault();
-  currNode.value === NODE.I ? I.moveLeft() : O.moveLeft();
+  tetromino().moveLeft();
 });
 onKeyStroke('ArrowDown', (e) => {
   e.preventDefault();
@@ -64,9 +72,7 @@ onKeyStroke('ArrowDown', (e) => {
 });
 
 function isGuide(rowIdx: number, colIdx: number) {
-  return currNode.value === NODE.I
-    ? !!I.endPosition.value.find(([r, c]) => r === rowIdx && c === colIdx)
-    : O.endPosition.value.find(([r, c]) => r === rowIdx && c === colIdx);
+  return tetromino().endPosition.value.find(([r, c]) => r === rowIdx && c === colIdx);
 }
 // onRenderTriggered(() => {
 //   console.log('render');
@@ -75,18 +81,22 @@ function isGuide(rowIdx: number, colIdx: number) {
 
 <template>
   <div>Tetris</div>
-  {{ I.endPosition }}
   <button @click="startGame">Start</button>
-  <button @click="I.fall">Fall</button>
-  <div>
-    <p>Current Node: {{ currNode }}</p>
-  </div>
+  <br />
   <div class="mx-auto flex">
     <div v-if="nextNode === NODE.I" class="grid gap-px">
-      <div v-for="num in 4" :key="num" class="w-8 h-8 bg-blue-200"></div>
+      <div v-for="num in 4" :key="num" class="w-8 h-8 bg-sky-500"></div>
+    </div>
+    <div v-if="nextNode === NODE.J" class="grid gap-px grid-cols-2">
+      <div
+        v-for="num in 6"
+        :key="num"
+        class="w-8 h-8"
+        :class="{ 'bg-blue-500': num === 5 || num % 2 === 0 }"
+      ></div>
     </div>
     <div v-if="nextNode === NODE.O" class="grid gap-px grid-cols-2">
-      <div v-for="num in 4" :key="num" class="w-8 h-8 bg-red-200"></div>
+      <div v-for="num in 4" :key="num" class="w-8 h-8 bg-red-500"></div>
     </div>
   </div>
   <div class="flex justify-center items-center">
@@ -100,8 +110,9 @@ function isGuide(rowIdx: number, colIdx: number) {
           :key="indexC"
           class="bg-white border border-transparent"
           :class="{
-            'bg-blue-200': node === NODE.I || node === NODE.FOSSIL_I,
-            'bg-red-200': node === NODE.O || node === NODE.FOSSIL_O,
+            'bg-sky-500': node === NODE.I || node === NODE.FOSSIL_I,
+            'bg-blue-500': node === NODE.J || node === NODE.FOSSIL_J,
+            'bg-red-500': node === NODE.O || node === NODE.FOSSIL_O,
             'border-red-700': isGuide(indexR, indexC),
           }"
           :style="{ width: `${NODE_SIZE}px`, height: `${NODE_SIZE}px` }"
