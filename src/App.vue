5b8d82ac196f8ef9088a542787203411635e5b23
Jon Ganebski <jon.ganebski@gmail.com>
2022-04-10 20:25:07 +0900

    :bug: fix ERR_UNSUPPORTED_DIR_IMPORT when build

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,25 @@
 <script setup lang="ts">
+import type { Component } from 'vue';
 import { IssueModalProvider } from './libs/issue';
+
+// ==================================================
+// This codes fixes following error on build time.
+//
+// (node:3606) UnhandledPromiseRejectionWarning: Error [ERR_UNSUPPORTED_DIR_IMPORT]: Directory import
+// '/home/jonganebski/github/jonganebski.github.io/node_modules/vue-query/devtools' is not supported
+// resolving ES modules imported from /home/jonganebski/github/jonganebski.github.io/.vite-ssg-temp/main.mjs
+//
+const VueQueryDevTools = shallowRef<Component | null>(null);
+//
+onMounted(async () => {
+  VueQueryDevTools.value = (await import('vue-query/devtools')).VueQueryDevTools;
+});
+// ==================================================
 </script>
 
 <template>
   <IssueModalProvider>
     <router-view />
   </IssueModalProvider>
+  <component :is="VueQueryDevTools" />
 </template>
