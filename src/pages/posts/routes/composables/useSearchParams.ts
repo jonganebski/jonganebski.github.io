c06b76228c4302ac9388f00750822f5213d5e481
Jon Ganebski <jon.ganebski@gmail.com>
2022-04-12 12:23:04 +0900

    :adhesive_bandage: replace useUrlSearchParams
    useUrlSearchParams occurs route warning time to time. Not sure why.

--- src/pages/posts/routes/composables/useSearchParams.ts
+++ src/pages/posts/routes/composables/useSearchParams.ts
@@ -1,26 +1,28 @@
 import { RoutesPostMeta } from '~/libs/markdown';
 
-interface SearchParams {
-  country?: string | string[];
-}
-
 export type CountryParam = string;
 
-const params = useUrlSearchParams<SearchParams>('history');
-
-const countryParam = computed<CountryParam>(() =>
-  !params.country ? 'all' : Array.isArray(params.country) ? params.country[0] : params.country,
-);
-
-function filterBySearchParams({ countries }: RoutesPostMeta): boolean {
-  if (countryParam.value === 'all') return true;
-  return countries.includes(countryParam.value);
-}
-
-function changeParam(code: string) {
-  params.country = code;
-}
-
 export function useSearchParams() {
+  const router = useRouter();
+  const route = useRoute();
+
+  const countryParam = computed<CountryParam>(() => {
+    if (!route.query.country) return 'all';
+    if (Array.isArray(route.query.country)) {
+      const firstParam = route.query.country[0];
+      return firstParam?.toString() ?? 'all';
+    }
+    return route.query.country.toString();
+  });
+
+  function filterBySearchParams({ countries }: RoutesPostMeta): boolean {
+    if (countryParam.value === 'all') return true;
+    return countries.includes(countryParam.value);
+  }
+
+  function changeParam(code: string) {
+    router.replace({ query: { country: code } });
+  }
+
   return { filterBySearchParams, changeParam, countryParam };
 }
