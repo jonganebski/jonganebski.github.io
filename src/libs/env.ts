1c548ad4297e105b5ebd69ea2e3fea7296aa282e
Jon Ganebski <jon.ganebski@gmail.com>
2022-04-13 14:44:08 +0900

    :bulb: add comments

--- src/libs/env.ts
+++ src/libs/env.ts
@@ -1,5 +1,22 @@
+/**
+ * @file
+ * This file handles environment variables.
+ */
+
+/**
+ * @name VITE_SUPABASE_URL
+ * @link https://supabase.com
+ */
 export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
 
+/**
+ * @name VITE_SUPABASE_ANON_KEY
+ * @link https://supabase.com
+ */
 export const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
 
+/**
+ * @name VITE_MAPBOX_ACCESS_TOKEN
+ * @link https://www.mapbox.com
+ */
 export const mapboxAccessToken = import.meta.env.VITE_MAPBOX_ACCESS_TOKEN as string;
