c3b63f0b7bbf8ac7bdacea68b3efa78b327de929
Jon Ganebski <jon.ganebski@gmail.com>
2022-04-23 22:09:00 +0900

    :sparkles: add router scroll behavior by scrollTo query

--- src/router.ts
+++ src/router.ts
@@ -5,5 +5,13 @@ import type { RouterOptions } from 'vite-ssg';
 export const routerOptions: RouterOptions = {
   routes: setupLayouts(generatedRoutes),
   base: import.meta.env.BASE_URL,
-  scrollBehavior: (_, __, savedPosition) => savedPosition ?? { top: 0 },
+  scrollBehavior: async (to, __, savedPosition) => {
+    await nextTick();
+    const { scrollTo } = to.query;
+    if (scrollTo) {
+      const el = document.getElementById(scrollTo.toString());
+      return { el, top: 60 };
+    }
+    return savedPosition ?? { top: 0 };
+  },
 };
